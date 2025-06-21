ca905d3a5bd2743ffb9229479b81e9901328d315
bellion13 <ttai132002@gmail.com>
2025-06-21 10:20:49 +0700

    Done category

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,28 @@ import Detail from './Pages/Detail/Detail';
 import Cart from './Pages/Cart/Cart';
 import Login from './Pages/Login/Login';
 import Footer from './Components/Footer/Footer';
+import men_banner from "./Assets/banner_mens.png";
+import women_banner from "./Assets/banner_women.png";
+import kid_banner from "./Assets/banner_kids.png";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Shop/>}/>
-          <Route path="/mens" element={<Category/>} />
-          <Route path="/womens" element={<Category/>} />
-          <Route path="/kids" element={<Category/>} />
-          <Route path="/detail" element={<Detail/>}>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<Category banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<Category banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<Category banner={kid_banner} category="kid" />} />
+
+          <Route path="/detail" element={<Detail />}>
             <Route path=":detailId" element={<Detail />} />
           </Route>
-          <Route path="/login" element={<Login/>} />
-          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
         <Footer />
-      
+
 
       </BrowserRouter>
     </div>
--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 import './Category.css'
-const Category = () => {
+import all_product from "../../Assets/all_product"
+import dropdown_icon from "../../Assets/dropdown_icon.png"
+import Item from '../../Components/Item/Item'
+
+const Category = (props) => {
+  // Lọc sản phẩm theo category từ props
+  const filteredProducts = all_product.filter(
+    (item) => item.category === props.category
+  );
   return (
-    <div>Category</div>
+    <div className='container'>
+      <div className='category'>
+        <img className='category__img' src={props.banner} alt="" />
+        <div className='category__indexSort'>
+          <p>
+            <span>
+              Show 1-{filteredProducts.length}
+            </span>
+            &nbsp;out of {filteredProducts.length} results
+          </p>
+          <div className='category__sort'>
+            Sort by <img src={dropdown_icon} alt="" />
+          </div>
+        </div>
+        <div className='category__products'>
+          {filteredProducts.map((item, i) => (
+            <Item
+              key={i}
+              id={item.id}
+              image={item.image}
+              name={item.name}
+              new_price={item.new_price}
+              old_price={item.old_price}
+              category={item.category}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
