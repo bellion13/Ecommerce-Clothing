09f394ce24bb37249d23e0a4f7b6e33a6a830fa2
AmBer <ttai132002@gmail.com>
2025-10-22 21:10:59 +0700

    -

--- vite.config.js
+++ vite.config.js
@@ -7,17 +7,30 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+    extensions: ['.js', '.jsx', '.json', '.png'],
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
   },
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       },
     },
   },
